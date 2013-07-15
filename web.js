5a7dcac15b02db6b4708668ee36c1c71b6b70031
Ubuntu <ubuntu@ip-172-31-15-109.us-west-2.compute.internal>
2013-07-15 16:17:02 +0000

    web.js

--- web.js
+++ web.js
@@ -20,5 +20,9 @@ console.log(fileBuf.toString());
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
+
+var fileBuf = fs.readFileSync('index.html');
+console.log(fileBuf.toString());
+
   console.log("Listening on " + port);
 });
